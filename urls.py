c076164f7a2b18d6edca3fbd9a6320ba2c555712
chj <chjfth@gmail.com>
2018-06-02 18:26:19 +0800

    [PENDING Q]
    You have 1 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): auth.
    
    > python manage.py showmigrations
    admin
     [X] 0001_initial
     [X] 0002_logentry_remove_auto_add
    auth
     [X] 0001_initial
     [X] 0002_alter_permission_name_max_length
     [X] 0003_alter_user_email_max_length
     [X] 0004_alter_user_username_opts
     [X] 0005_alter_user_last_login_null
     [X] 0006_require_contenttypes_0002
     [X] 0007_alter_validators_add_error_messages
     [ ] 0008_alter_user_username_max_length
    books
     [X] 0001_initial
    contenttypes
     [X] 0001_initial
     [X] 0002_remove_content_type_name
    sessions
     [X] 0001_initial
    
    但如何查出 pending migration 具体要用的 SQL 语句呢?

--- urls.py
+++ urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 #from django.views.generic import TemplateView
 
